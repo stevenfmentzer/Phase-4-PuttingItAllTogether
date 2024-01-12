6cadd08008cd23e883886b983feb22beacb89136
Eleanor Kelman <ekelman3@gmail.com>
2024-01-12 10:04:26 -0500

    finished lecture instructions

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Phase-4-PuttingItAllTogether
+
+1. Fill out the DB models as per the DB schema.
+2. Add relationships and serialization rules to the models.
+3. Add GET routes to `/makeup_items`, `/stores`, `/stores/<int:id>`
+4. Add DELETE route to `/stores/<int:id>`
+5. Add POST route to `/stores`
+6. Add PATCH route to `/store_has_item/<int:id>`
--- server/seed.py
+++ server/seed.py
@@ -4,4 +4,75 @@ from models import db, MakeupItem, Store, StoreHasItem
 
 if __name__ == '__main__':
     with app.app_context():
-        pass
+        
+        print("Clearing Out Tables...")
+
+        MakeupItem.query.delete()
+        Store.query.delete()
+        StoreHasItem.query.delete()
+
+        print("Seeding MakeupItem model...")
+
+        new_makeup_items = [
+            MakeupItem(
+                name = "SatinAllure Lipstick",
+                brand = "Pat McGrath Labs",
+                type = "lipstick"
+            ),
+            MakeupItem(
+                name = "Mini Soft Pinch Liquid Blush",
+                brand = "Rare Beauty by Selena Gomez",
+                type = "blush"
+            ),
+            MakeupItem(
+                name = "They're Real! Lengthening Mascara",
+                brand = "Benefit Cosmetics",
+                type = "mascara"
+            )
+        ]
+
+        db.session.add_all(new_makeup_items)
+
+        print("Seeding Store model...")
+
+        new_stores = [
+            Store(
+                name = "Sephora"
+            ),
+            Store(
+                name = "Ulta"
+            )
+        ]
+
+        db.session.add_all(new_stores)
+
+        db.session.commit()
+
+        print("Seeding StoreHasItem model...")
+
+        new_joins = [
+            StoreHasItem(
+                item_id = new_makeup_items[0],
+                store_id = new_stores[0],
+                price = 30.00
+            ),
+            StoreHasItem(
+                item_id = new_makeup_items[0],
+                store_id = new_stores[1],
+                price = 30.00
+            ),
+            StoreHasItem(
+                item_id = new_makeup_items[1],
+                store_id = new_stores[0],
+                price = 14.00
+            ),
+            StoreHasItem(
+                item_id = new_makeup_items[2],
+                store_id = new_stores[1],
+                price = 29.00
+            )
+        ]
+
+        db.session.add_all(new_joins)
+
+        db.session.commit()
